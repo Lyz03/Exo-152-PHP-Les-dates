b1cd9a734a087d8bc83ec8f1d1e036a9429e5368
Lyz03 <lizoe@netcourrier.com>
2022-01-12 11:01:51 +0100

    exo 152

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 // Premiere ligne
 
+$date = getdate();
+echo $date["weekday"] . "<br>";
 
+$date2 = strtotime("2018/12/10");
+echo date("Y.m.d", $date2) . "<br>";
 
-
-
-
+$date3 = mktime(11, 35, 07, 0, 0, 0);
+echo date("H.i.s", $date3) . "<br>";
